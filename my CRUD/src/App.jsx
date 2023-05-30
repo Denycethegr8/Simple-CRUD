e06b4277aa4e12ed202c0203e81d9b8ef04abcf8
Denycethegr8 <jini3209@gmail.com>
2023-05-30 20:42:09 +0900

    finish up the logic

--- my CRUD/src/App.jsx
+++ my CRUD/src/App.jsx
@@ -6,19 +6,57 @@ function App() {
   const [todoList, setTodoList] = useState([]);
 
   const handleChange = (event) => {
-    setAddTask(event.target.value)
-  }
+    setAddTask(event.target.value);
+  };
 
   const handleClick = () => {
-    
+    const taskProps = {
+      id: todoList.length + 1,
+      taskName: addTask,
+      isDone: false,
+    };
+
+    setTodoList((prevTodoList) => [...prevTodoList, taskProps]);
+
+    document.getElementById('inputText').value = "";
+  };
+
+  const deleteTask = (taskID) => {
+    setTodoList(todoList.filter(task => task.id !== taskID))
   }
 
+  const complete = (taskID) => {
+    setTodoList(todoList.map((task) => {
+      return task.id === taskID? {...task, isDone:true} : task
+    }
+    ))
+  }
 
+  const newTask = todoList.map((task) => (
+    <ul key={task.id} className="list-group list-group-flush">
+      <li className="list-group-item" style={{textDecorationLine:task.isDone? "line-through":"none"}}>
+        {task.taskName}
+        <div
+          className="btn-group btn-group-sm"
+          role="group"
+          aria-label="Basic example"
+        >
+          <button type="button" className="btn btn-outline-success" onClick={()=> complete(task.id)}>
+            DONE
+          </button>
+          <button type="button" className="btn btn-outline-secondary" onClick={() => deleteTask(task.id)}>
+            DELETE
+          </button>
+        </div>
+      </li>
+    </ul>
+  ));
 
   return (
-      <>
+    <>
       <div className="input-group mb-3">
         <input
+          id="inputText"
           type="text"
           className="form-control"
           placeholder="Things to do"
@@ -30,29 +68,12 @@ function App() {
           className="btn btn-outline-secondary"
           type="button"
           id="button-addon2"
-          onChange={handleClick}
+          onClick={handleClick}
         >
           ADD
         </button>
       </div>
-
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">
-          An item
-          <div
-            className="btn-group btn-group-sm"
-            role="group"
-            aria-label="Basic example"
-          >
-            <button type="button" className="btn btn-outline-success">
-              DONE
-            </button>
-            <button type="button" className="btn btn-outline-secondary">
-              DELETE
-            </button>
-          </div>
-        </li>
-      </ul>
+      {newTask}
     </>
   );
 }
